bef06f8a2a296b37e8e8d62a985aec4679dae651
Ade Novid <adenvt@gmail.com>
2024-07-23 14:21:22 +0700

    fix(ghostscript): validate rpc call name

--- packages/ghoulscript/src/rpc/call.ts
+++ packages/ghoulscript/src/rpc/call.ts
@@ -2,12 +2,15 @@ import * as core from '../core'
 
 export type Core = typeof core
 
-export type Commands = keyof Core
+export type Commands = keyof Core & string
 
 export type CommandArgs<C extends Commands> = Parameters<Core[C]>
 
 export type CommandResult<C extends Commands> = ReturnType<Core[C]>
 
 export async function callRPC<C extends Commands> (name: C, args: CommandArgs<C>): Promise<CommandResult<C>> {
+  if (typeof core[name] !== 'function')
+    throw new TypeError(`Invalid action: "${name}"`)
+
   return await (core[name] as (...args: any[]) => Promise<any>)(...args)
 }
